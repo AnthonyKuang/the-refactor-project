132a949e9922be9d1a6cd297cad735089d7908ab
Anthony Kuang <kuanganthony@gmail.com>
2021-01-24 18:09:05 -0800

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,12 @@ export default function Home() {
         <title>The Refactor Project</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <div className='container bg-yellow-50 dark:bg-yellow-900'>
+        <h1 className='text-4xl font-light dark:text-white'>
+          The Refactor Project
+        </h1>
+      </div>
     </div>
   );
 }
