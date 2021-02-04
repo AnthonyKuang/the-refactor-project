7a92304fa805b5c6227b6583d0721c1e48ced490
Anthony Kuang <kuanganthony@gmail.com>
2021-02-03 18:30:54 -0800

    positioned images

--- pages/index.js
+++ pages/index.js
@@ -29,20 +29,23 @@ export default function Home() {
         </a>
       </section>
 
-      <main className='mt-60'>
-        <div className='container-homepage'>
-          <p className='gradient-text text-xs sm:text-sm lg:text-base'>
-            Design
-          </p>
-          <h2 className='text-3xl md:text-4xl mt-4'>
-            Ideation and wireframing
-          </h2>
-          <p className='showcase-text mt-8'>
-            Our design team will create a mockup of your organization's website
-            using web design software such as Figma and Adobe XD. Review our
-            work and offer improvements. Do not settle until you're satisfied.
-          </p>
-          <div className='shadow-xl inline-block'>
+      <main className='mt-48'>
+        <div className='container-homepage flex items-center'>
+          <div className='max-w-md'>
+            <p className='gradient-text text-xs sm:text-sm lg:text-base'>
+              Design
+            </p>
+            <h2 className='text-3xl md:text-4xl mt-4'>
+              Ideation and wireframing
+            </h2>
+            <p className='mt-12 leading-loose'>
+              Our design team will create a mockup of your organization's
+              website using web design software such as Figma and Adobe XD.
+              Review our work and offer improvements. Do not settle until you're
+              satisfied.
+            </p>
+          </div>
+          <div className='shadow-xl relative left-48 transform scale-125'>
             <Image
               src='/style-guide.svg'
               alt='Style Guide'
@@ -51,21 +54,27 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className='container-homepage mt-24'>
-          <p className='gradient-text text-xs sm:text-sm lg:text-base'>Code</p>
-          <h2 className='text-3xl md:text-4xl mt-4'>Building and testing</h2>
-          <p className='showcase-text mt-8'>
-            Our programming team will make your design a reality. Using
-            cutting-edge technologies such as Next.js and TailwindCSS, your
-            website will be optimized with world-class performance, best
-            practices, and SEO.
-          </p>
-          <Image
-            src='/code-showcase.svg'
-            alt='Code Showcase'
-            width={1698}
-            height={1336}
-          />
+        <div className='mt-72 container-homepage flex items-center'>
+          <div className='max-w-md'>
+            <p className='gradient-text text-xs sm:text-sm lg:text-base'>
+              Code
+            </p>
+            <h2 className='text-3xl md:text-4xl mt-4'>Building and testing</h2>
+            <p className='leading-loose mt-12'>
+              Our programming team will make your design a reality. Using
+              cutting-edge technologies such as Next.js and TailwindCSS, your
+              website will be optimized with world-class performance, best
+              practices, and SEO.
+            </p>
+          </div>
+          <div className='relative left-44 transform scale-110'>
+            <Image
+              src='/code-showcase.svg'
+              alt='Code Showcase'
+              width={1698}
+              height={1336}
+            />
+          </div>
         </div>
         <div className='container-homepage mt-24'>
           <p className='gradient-text text-xs sm:text-sm lg:text-base'>
@@ -74,7 +83,7 @@ export default function Home() {
           <h2 className='text-3xl md:text-4xl mt-4'>
             Enjoy your Refactored website!
           </h2>
-          <p className='showcase-text mt-8'>
+          <p className='leading-loose mt-12'>
             We will deploy the website to the web through Vercel, allowing you
             to see the final result. We guarantee that your web metric scores
             will improve. In the end, it is all up to your choice whether or not
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: 'class', // or 'media' or 'class'
+  important: true,
   theme: {
     colors: {
       transparent: 'transparent',
